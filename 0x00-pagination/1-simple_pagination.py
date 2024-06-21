f4b7105d5fa0adce31955ef6e3ff15566a682f43
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-06-21 06:46:20 -0700

    1-simple_pagination.py

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""pagination helper function"""
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """retrieve index range"""
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+
+
+class Server:
+    """server class"""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """retrieve a page of data"""
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]

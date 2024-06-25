3b179607b5e3cc37683895a5da93459a64a0c0fb
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-06-25 10:51:29 -0700

    2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -18,9 +18,10 @@ class LIFOCache(BaseCaching):
         """assign to dictionary"""
         if key is None or item is None:
             return
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            last_key, _ = self.cache_data.popitem(True)
-            print("DISCARD:", last_key)
+        if key not in self.cache_data:
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
         self.cache_data[key] = item
         self.cache_data.move_to_end(key, last=True)
 

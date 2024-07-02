78c4b9dc27b24304472c5e2fb441db5059724504
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-07-02 05:50:38 -0700

    3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -39,6 +39,11 @@ def index() -> str:
     """
     return render_template("3-index.html")
 
+# uncomment this line and comment the @babel.localeselector
+# you get this error:
+# AttributeError: 'Babel' object has no attribute 'localeselector'
+# babel.init_app(app, locale_selector=get_locale)
+
 
 if __name__ == "__main__":
     app.run()

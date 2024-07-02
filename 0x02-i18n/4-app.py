dc55c525be343113fc52406d6a2a13637de22472
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-07-02 06:17:08 -0700

    4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""task 0 basic Flask app"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """config class"""
+
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """retrieve the loacle for web page
+
+    Return:
+        str: best match
+    """
+    locale = request.args.get('locale')
+    if loacle in app.config['LANGUAGES']:
+        return loacle
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """default route
+
+    Return:
+        html: homepage
+    """
+    return render_template("4-index.html")
+
+# uncomment this line and comment the @babel.localeselector
+# you get this error:
+# AttributeError: 'Babel' object has no attribute 'localeselector'
+# babel.init_app(app, locale_selector=get_locale)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""task 0 basic Flask app"""
+
+from typing import Dictt, Union
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config:
+    """config class"""
+
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """retrieve a user based on user id"""
+    login_id = requests.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """perform some rroutine before each request"""
+
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """retrieve the loacle for web page
+
+    Return:
+        str: best match
+    """
+    locale = request.args.get('locale')
+    if loacle in app.config['LANGUAGES']:
+        return loacle
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """default route
+
+    Return:
+        html: homepage
+    """
+    return render_template("4-index.html")
+
+# uncomment this line and comment the @babel.localeselector
+# you get this error:
+# AttributeError: 'Babel' object has no attribute 'localeselector'
+# babel.init_app(app, locale_selector=get_locale)
+
+
+if __name__ == "__main__":
+    app.run()

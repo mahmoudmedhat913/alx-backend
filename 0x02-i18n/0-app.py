9d0ae0e4fd1c7414431c0d148527e79def9896f0
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-07-02 03:27:05 -0700

    0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""task 0 basic Flask app"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """default route"""
+    return render_template("0-index.html",)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
